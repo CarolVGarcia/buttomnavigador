2332ee9bbf6df13c38ac4a5a5d0c185739bc3df6
Carol García <jake.v.garcia.mazatlan@gmail.com>
2024-06-25 12:15:11 -0700

    se agrego el codigo al listafragment

--- app/src/main/java/com/example/appmenubuttom92/ListaFragment.kt
+++ app/src/main/java/com/example/appmenubuttom92/ListaFragment.kt
@@ -5,55 +5,46 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import androidx.appcompat.app.AlertDialog
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ListaFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ListaFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var listView: ListView
+    private lateinit var arrayList: ArrayList<String>
+    private lateinit var adapter: ArrayAdapter<String>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_lista, container, false)
+        val view = inflater.inflate(R.layout.fragment_lista, container, false)
+        listView = view.findViewById(R.id.lstAlumnos)
+
+        val items = resources.getStringArray(R.array.alumnos)
+
+        arrayList = java.util.ArrayList()
+        arrayList.addAll(items)
+
+        adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, arrayList)
+
+        listView.adapter = adapter
+
+        listView.setOnItemClickListener({parent, view, position, id ->
+            var alumno:String = parent.getItemIdAtPosition(position).toString()
+            var builder = AlertDialog.Builder(requireContext())
+
+            builder.setTitle("Lista de alumnos")
+            builder.setMessage(position.toString() + ":" + alumno)
+            builder.setPositiveButton("OK"){ dialog, which ->
+
+            }
+
+        })
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ListaFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ListaFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
