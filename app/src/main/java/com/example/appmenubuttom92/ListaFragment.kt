baf62c213ed8d1f8eb1e06c1a76038ca37e5fbd6
Carol García <jake.v.garcia.mazatlan@gmail.com>
2024-06-25 13:01:58 -0700

    se agrego lo visto en classroom

--- app/src/main/java/com/example/appmenubuttom92/ListaFragment.kt
+++ app/src/main/java/com/example/appmenubuttom92/ListaFragment.kt
@@ -11,40 +11,41 @@ import androidx.appcompat.app.AlertDialog
 
 
 class ListaFragment : Fragment() {
-
     private lateinit var listView: ListView
     private lateinit var arrayList: ArrayList<String>
     private lateinit var adapter: ArrayAdapter<String>
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_lista, container, false)
+
         listView = view.findViewById(R.id.lstAlumnos)
 
         val items = resources.getStringArray(R.array.alumnos)
 
-        arrayList = java.util.ArrayList()
+        arrayList = ArrayList()
         arrayList.addAll(items)
 
         adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, arrayList)
 
         listView.adapter = adapter
 
-        listView.setOnItemClickListener({parent, view, position, id ->
-            var alumno:String = parent.getItemIdAtPosition(position).toString()
-            var builder = AlertDialog.Builder(requireContext())
-
-            builder.setTitle("Lista de alumnos")
-            builder.setMessage(position.toString() + ":" + alumno)
+        listView.setOnItemClickListener { parent, view, position, id ->
+            var alumno:String = parent.getItemAtPosition(position).toString()
+            val builder = AlertDialog.Builder(requireContext())
+            builder.setMessage(position.toString()+ ":" + alumno)
             builder.setPositiveButton("OK"){ dialog, which ->
 
             }
-
-        })
+            builder.show()
+        }
 
         return view
-    }
 
+    }
 }
+
