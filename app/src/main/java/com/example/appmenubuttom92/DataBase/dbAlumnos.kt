fb877d83e9dae3923e866c8cda09df9816598bff
Carol García <jake.v.garcia.mazatlan@gmail.com>
2024-06-28 12:59:00 -0700

    se agrego el packet database

---
+++ app/src/main/java/com/example/appmenubuttom92/DataBase/dbAlumnos.kt
@@ -0,0 +1,11 @@
+package com.example.appmenubuttom92.DataBase
+
+import android.content.Context
+
+class dbAlumnos (private val context: Context){
+
+    private val dbHelper : AlumnosDbHelper = AlumnosDbHelper(context)
+    private
+
+
+}
